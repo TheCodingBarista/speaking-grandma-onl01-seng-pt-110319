5fc90ad8f2747f59040ba2eca9a56c17b0f179f3
Melissa Holt <melissa.holt21@gmail.com>
2019-11-06 23:22:10 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,12 +1,12 @@
 def speak_to_grandma(phrase)
-  if phrase
+  if phrase.downcase
    puts "HUH?! SPEAK UP, SONNY!" 
   elsif phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   else
     puts "NO, NOT SINCE 1938!"
   end
-#end
+speak_to_grandma(phrase)
 
 
 
